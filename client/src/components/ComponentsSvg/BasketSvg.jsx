ba601c6531e4a4b3fe369bfa70d4ab5825e64630
Vadya <kosmat3936@gmail.com>
2023-05-18 20:31:25 +0300

    some fixes in header

--- client/src/components/ComponentsSvg/BasketSvg.jsx
+++ client/src/components/ComponentsSvg/BasketSvg.jsx
@@ -3,8 +3,8 @@ import * as React from "react"
 export const BasketSvg = (props) => {
   return (
     <svg
-      width={20}
-      height={20}
+      width={30}
+      height={30}
       viewBox="0 0 20 20"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
